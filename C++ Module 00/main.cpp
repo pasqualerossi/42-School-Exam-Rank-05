0e3674840eeed04288a2b63cebbf225960addcfe
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-12-09 16:54:09 +1030

    Update main.cpp

--- C++ Module 00/main.cpp
+++ C++ Module 00/main.cpp
@@ -3,16 +3,15 @@
 int main()
 {
   Warlock const richard("Richard", "Mistress of Magma");
-  richard.introduce();
-  std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+    richard.introduce();
+      std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
 
-  Warlock* jack = new Warlock("Jack", "the Long");
-  jack->introduce();
-  jack->setTitle("the Mighty");
-  jack->introduce();
+        Warlock* jack = new Warlock("Jack", "the Long");
+	  jack->introduce();
+	    jack->setTitle("the Mighty");
+	      jack->introduce();
 
-  delete jack;
+	        delete jack;
 
-  return (0);
-
+		  return (0);
 }
