fba1e2fbaf4edd04ca5de5151806d845cb830789
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-06-11 18:59:32 +0930

    Update Fwoosh.cpp

--- cpp_module_01/Fwoosh.cpp
+++ cpp_module_01/Fwoosh.cpp
@@ -1,12 +1,8 @@
 #include "Fwoosh.hpp"
 
-Fwoosh::Fwoosh() : ASpell("Fwoosh", "fwooshed")
-{
-}
+Fwoosh::Fwoosh() : ASpell("Fwoosh", "fwooshed") {}
 
-Fwoosh::~Fwoosh()
-{
-}
+Fwoosh::~Fwoosh() {}
 
 ASpell* Fwoosh::clone() const
 {
